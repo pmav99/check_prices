9481cbb2ddc563fcca9b9ede7dbd3e323b890707
Panos Mavrogiorgos <pmav99@gmail.com>
2014-06-17 01:12:24 +0300

    Fix gmail. gmail needs encryption

--- email_servers/gmail.py
+++ email_servers/gmail.py
@@ -16,5 +16,7 @@ PORT = 587
 def get_server(username, password):
     """ Return a server for gmx.com. """
     server = smtplib.SMTP(SERVER, PORT)
+    server.ehlo()
+    server.starttls()
     server.login(username, password)
     return server
